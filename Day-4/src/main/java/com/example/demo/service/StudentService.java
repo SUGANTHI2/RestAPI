a65321205005e4f7f1f8dc64c62832791b67de11
SUGANTHI2 <gsuganthi2603@gmail.com>
2023-02-24 12:18:54 +0530

    RestAPI

---
+++ Day-4/src/main/java/com/example/demo/service/StudentService.java
@@ -0,0 +1,36 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+
+@Service
+public class StudentService {
+	@Autowired
+	StudentRepository repository;
+	
+	public boolean addStudent(Student student) {
+		repository.save(student);
+		return true;
+	}
+	public List<Student> getStudent(){
+		return repository.findAll();
+	}
+	
+	public Optional<Student> getStudentById(int id){
+		return repository.findById(id);
+	}
+	public Student updateStudentById(Student student) {
+		repository.save(student);
+		return student;
+	}
+	public boolean deleteStudentById(int id) {
+		repository.deleteById(id);
+		return true;
+	}
+}
